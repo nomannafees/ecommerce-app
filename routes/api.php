eb2e4a8d29a9bd626250ae0508df7ce5c8aa510f
sikandar-hexa <sikandar@hexalogicx.com>
2026-09-05 14:53:15 +0500

    fixes

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\CategoryProduct;
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\BannerController;
+use App\Models\AdminStore;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
