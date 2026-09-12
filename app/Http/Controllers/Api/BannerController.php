162bcfd33c8e3fb071f517637bb0b5b2e53406a1
sikandar-hexa <sikandar@hexalogicx.com>
2026-09-12 15:48:56 +0500

    fixes

--- app/Http/Controllers/Api/BannerController.php
+++ app/Http/Controllers/Api/BannerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdminStore;
 use Illuminate\Http\Request;
 use App\Models\BrandsBanner;
 use App\Models\FeaturedBanner;
